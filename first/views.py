3ba8735d0129146269ef25392fe1641507ddd878
Arthur Chang <arthurc0102@gmail.com>
2024-10-27 11:42:38 +0800

    feat: add say_hello_template

--- first/views.py
+++ first/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def message_view(request):
@@ -9,3 +10,7 @@ def say_hello(request):
     get_parms = request.GET  # 透過 Http 傳遞進來的 GET 參數
     name = get_parms.get("name", "unknown")  # 從 GET 參數（dict）中尋找 name 的值
     return HttpResponse(f"Hello {name}")
+
+
+def say_hello_template(request):
+    return render(request, "say_hello.html")
