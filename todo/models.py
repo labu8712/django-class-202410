7fd5537879684a874ee439ffae313ce150dc9639
Arthur Chang <arthurc0102@gmail.com>
2024-11-03 10:18:07 +0800

    Create project and item model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
