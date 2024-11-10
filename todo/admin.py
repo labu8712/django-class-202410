af3bc36eea044a6c91ef8e4c40be5d9b5811c0b0
Arthur Chang <arthurc0102@gmail.com>
2024-11-10 10:12:34 +0800

    Update settings and admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Item, Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_completed", "project")
+    search_fields = ("title", "content")
+    list_filter = ("project", "is_completed")
+
+    save_as = True
